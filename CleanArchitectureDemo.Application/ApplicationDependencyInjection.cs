b584b0d7e8c958a6242998807851ba67d64efe77
Obadiah Korir <korir400@gmail.com>
2024-09-19 21:42:32 +0300

    first commit

---
+++ CleanArchitectureDemo.Application/ApplicationDependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureDemo.Application
+{
+    public static class ApplicationDependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            var valassembly = typeof(DependencyInjectionExtensions).Assembly;
+            services.AddValidatorsFromAssembly(valassembly);
+
+            return services;
+        }
+    }
+}
